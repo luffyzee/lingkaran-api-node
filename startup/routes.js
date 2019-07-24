87cd9ab7da00c9431635c4d20e2c20551b70da92
Sunarya <luffyzeee@gmail.com>
2019-07-24 16:45:46 +0700

    add cors

--- startup/routes.js
+++ startup/routes.js
@@ -1,23 +1,11 @@
-const cors = require("cors");
 const express = require("express");
-const genres = require("../routes/genres");
-const customers = require("../routes/customers");
-const movies = require("../routes/movies");
-const rentals = require("../routes/rentals");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
-const returns = require("../routes/returns");
 const error = require("../middleware/error");
 
 module.exports = function(app) {
-  app.use(cors());
   app.use(express.json());
-  app.use("/genres", genres);
-  app.use("/customers", customers);
-  app.use("/movies", movies);
-  app.use("/rentals", rentals);
-  app.use("/users", users);
-  app.use("/auth", auth);
-  app.use("/returns", returns);
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
   app.use(error);
 };
